f17c34b5b954856a00e2f2859599f0ec4efe7002
Conrad Lo <email@conradlo.com>
2022-01-28 15:15:17 +0800

    refactor: use number namespace parse int

--- src/color.ts
+++ src/color.ts
@@ -1,8 +1,8 @@
 const getRGB = (color: string) => {
   const hex = color.replace(/#/g, "");
-  const r = parseInt(hex.substring(0, 2), 16);
-  const g = parseInt(hex.substring(2, 4), 16);
-  const b = parseInt(hex.substring(4, 6), 16);
+  const r = Number.parseInt(hex.substring(0, 2), 16);
+  const g = Number.parseInt(hex.substring(2, 4), 16);
+  const b = Number.parseInt(hex.substring(4, 6), 16);
   return { r, g, b };
 };
 
